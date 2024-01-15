5903be6e8fcc364fe67651e4629c292e61e6b0f0
Siya-Mwol <109611007+Siya-Mwol@users.noreply.github.com>
2024-01-15 10:40:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "gay6bCrJ",
   LANG: process.env.LANG || "EN",
   HANDLERS:process.env.HANDLER === "false" || process.env.HANDLER === "null"? "^": "^",
   STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'ᴊᴀʀᴠɪꜱ;ᴡᴀʙᴏᴛ',
